6faecee26f17fd7611c582321a8c728757853509
Ashok Menon <amenon94@gmail.com>
2013-04-10 18:46:16 +0200

    Preventing me from making *that* mistake again.

--- config/initializers/gmail.rb
+++ config/initializers/gmail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => 'gmail.com',
-  :user_name            => 'lingua.confirm@gmail.com', # full email address (user@your.host.name.com)
-  :password             => 'herokuActionMailer',
+  :user_name            => 'your.email@gmail.com', # full email address (user@your.host.name.com)
+  :password             => 'yourpassword',
   :authentication       => 'plain',
   :enable_starttls_auto => true
 }
