6b38774764aceed8a7e7c71383a91356871f5d99
Ashok Menon <amenon94@gmail.com>
2013-04-10 17:43:58 +0100

    Saving myself from making *that* mistake again

--- config/initializers/gmail.rb
+++ config/initializers/gmail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => 'gmail.com',
-  :user_name            => 'lingua.confirm@gmail.com', # full email address (user@your.host.name.com)
-  :password             => 'teachmetouploadthistogithub',
+  :user_name            => 'your.email@here.com', # full email address (user@your.host.name.com)
+  :password             => 'yourpasswordhere',
   :authentication       => 'plain',
   :enable_starttls_auto => true
 }
