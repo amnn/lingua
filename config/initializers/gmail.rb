0c1284841c6c785a89d2377a8c5caf16dc3445ff
Ashok Menon <amenon94@gmail.com>
2013-04-10 17:40:38 +0100

    Changed actionmailer password

--- config/initializers/gmail.rb
+++ config/initializers/gmail.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.smtp_settings = {
   :port                 => 587,
   :domain               => 'gmail.com',
   :user_name            => 'lingua.confirm@gmail.com', # full email address (user@your.host.name.com)
-  :password             => 'herokuActionMailer',
+  :password             => 'teachmetouploadthistogithub',
   :authentication       => 'plain',
   :enable_starttls_auto => true
 }
